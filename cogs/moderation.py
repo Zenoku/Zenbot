0e79d0d83304cafe1172204e3b9dd1a30c2ecf31
Zenoku <theultimatebot0@gmail.com>
2021-05-23 14:40:12 -0400

    added unban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -9,8 +9,17 @@ class moderation(commands.Cog):
     # command is decorator for cogs
     @commands.command(aliases = ["sb"])
     async def selfban(self, ctx):
-        await ctx.guild.ban(ctx.author)
+        await ctx.guild.ban(ctx.author, reason = f"{ctx.author} banned themself", delete_message_days = 0)
         await ctx.send(f"{ctx.author} has banned themself")
+
+    @commands.command()
+    async def unban(self, ctx, user: discord.User):
+        if ctx.author.guild_permissions.ban_members:
+            await ctx.guild.unban(user)
+            await user.send(f"You have been unbanned from {ctx.guild}")
+            await ctx.send(f"{user} has been unbanned")
+        else:
+            await ctx.send("You don't have the permissions to do this")
 
     @commands.command(aliases = ["sk"])
     async def selfkick(self, ctx):
