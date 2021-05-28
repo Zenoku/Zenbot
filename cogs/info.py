e1c017dd7076507a9e10b49e32150f159db84a59
Zenoku <theultimatebot0@gmail.com>
2021-05-28 15:22:05 -0400

    changed order of serverinfo

--- cogs/info.py
+++ cogs/info.py
@@ -6,22 +6,18 @@ class info(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    # add embeds to this stuff
     @commands.command(aliases = ["ui"])
     async def userinfo(self, ctx, member:discord.User = None):
         if member == None:
             member = ctx.author
-            embed = discord.Embed(title = member)
-            embed.add_field(name = "Member ID", value = member.id, inline = True)
-            embed.add_field(name = "Creation date:", value = member.created_at, inline = True)
-            embed.set_image(url = member.avatar_url)
-            await ctx.send(embed = embed)
-        else:
-            embed = discord.Embed(title = member)
-            embed.add_field(name = "Member ID", value = member.id, inline = True)
-            embed.add_field(name = "Creation date:", value = member.created_at, inline = True)
-            embed.set_image(url = member.avatar_url)
-            await ctx.send(embed = embed)
+
+        embed = discord.Embed(title = member, color = member.color)
+        embed.add_field(name = "Member ID", value = member.id, inline = True)
+        embed.add_field(name = "Creation date", value = member.created_at, inline = True)
+        embed.add_field(name = "Color", value = member.color, inline = True)
+        embed.set_image(url = member.avatar_url)
+        await ctx.send(embed = embed)
+
     
     # add embeds and figure out role permission stuff
     @commands.command(aliases = ["ri"])
@@ -35,13 +31,13 @@ class info(commands.Cog):
     async def serverinfo(self, ctx):
         guild = ctx.guild
         embed = discord.Embed(title = guild)
-        embed.add_field(name = "Boosts", value = guild.premium_subscription_count, inline = True)
-        embed.add_field(name = "Created at", value = guild.created_at, inline = True)
-        embed.add_field(name = "Number of channels", value = len(guild.channels), inline = True)
+        embed.add_field(name = "Owner", value = guild.owner, inline = True)
         embed.add_field(name = "ID", value = guild.id, inline = True)
+        embed.add_field(name = "Created at", value = guild.created_at, inline = True)
+        embed.add_field(name = "Boosts", value = guild.premium_subscription_count, inline = True)
+        embed.add_field(name = "Number of channels", value = len(guild.channels), inline = True)
         embed.add_field(name = "Number of members", value = guild.member_count, inline = True)
         # idk why owner isn't returning. debug later
-        embed.add_field(name = "Owner", value = guild.owner, inline = True)
         embed.add_field(name = "Number of roles", value = len(guild.roles), inline = True)
         embed.set_image(url = guild.icon_url)
         await ctx.send(embed = embed)
